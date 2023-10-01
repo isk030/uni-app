a4dd523683dec9020ab7ae31e3d8094a9465c02b
Iskender Dilaver <idi@gtcsolutions.de>
2023-10-01 15:02:40 +0200

    test: test updated

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -10,7 +10,7 @@ vi.mock('public/logo.png', () => ({
     default: { src: '/logo.png' },
 }));
 
-test('renders canvas element', () => {
+test('renders main element', () => {
     // This is an example of how to ensure a specific item is in the document
     // But it's disabled by default (by test.skip) so the test doesn't fail
     // when you remove the default content from the page
@@ -18,6 +18,6 @@ test('renders canvas element', () => {
     // This is an example on how to mock api hooks when testing
 
     const { container } = render(<Home />);
-    const element = container.querySelector('aside');
+    const element = container.querySelector('main');
     expect(element).toBeInTheDocument();
 });
