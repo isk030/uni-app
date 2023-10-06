24977dd9b2cc13a53614b04ab7b1fcbfa83a4a56
Iskender Dilaver <idi@gtcsolutions.de>
2023-10-06 17:44:53 +0100

    feat: iteration for multiple ESAs

--- src/app/components/tabs.tsx
+++ src/app/components/tabs.tsx
@@ -12,60 +12,26 @@ import { FC, useState } from 'react';
 type TabElement = {
     label: string;
     value: string;
-    desc: string | JSX.Element;
+    desc: JSX.Element | string | undefined;
 };
 
 type TabData = {
     data: Array<TabElement>;
     initialActiveTab: string | JSX.Element;
-    menu?: JSX.Element;
+    tabOptions?: JSX.Element;
 };
 
 export const UnderlineTabs: FC<TabData> = ({
     data,
     initialActiveTab,
-    menu,
+    tabOptions,
 }) => {
     const [activeTab, setActiveTab] = useState(initialActiveTab || 'html');
-    const dataContainer = data || [
-        {
-            label: 'HTML',
-            value: 'html',
-            desc: `It really matters and then like it really doesn't matter.
-      What matters is the people who are sparked by it. And the people 
-      who are like offended by it, it doesn't matter.`,
-        },
-        {
-            label: 'React',
-            value: 'react',
-            desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-        },
-        {
-            label: 'Vue',
-            value: 'vue',
-            desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-        },
-        {
-            label: 'Angular',
-            value: 'angular',
-            desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-        },
-        {
-            label: 'Svelte',
-            value: 'svelte',
-            desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-        },
-    ];
+    const dataContainer = data || [];
 
     return (
         <Card className='h-[calc(100vh-2rem)] w-full my-4 p-4  shadow-xl shadow-blue-gray-900/5'>
-            {menu && menu}
+            {tabOptions && tabOptions}
             <br />
             <Tabs value={activeTab}>
                 <TabsHeader
@@ -91,7 +57,8 @@ export const UnderlineTabs: FC<TabData> = ({
                 <TabsBody>
                     {dataContainer.map(
                         ({ value, desc }) =>
-                            value && (
+                            value &&
+                            desc && (
                                 <TabPanel key={value} value={value}>
                                     {desc}
                                 </TabPanel>
--- src/app/graphical-visualisation-technologies/tabs.tsx
+++ src/app/graphical-visualisation-technologies/tabs.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { Option, Select } from '@material-tailwind/react';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { create } from 'zustand';
 import { UnderlineTabs } from '../components/tabs';
 
-const Task = () => {
+const TaskOne = () => {
     return (
         <div>
             <p className='font-bold'>Aufgabenstellung:</p>
@@ -59,59 +59,157 @@ const Task = () => {
         </div>
     );
 };
-const data = [
-    {
-        label: 'Aufgabenstellung',
-        value: 'task',
-        desc: <Task />,
-    },
-    {
-        label: 'Idee',
-        value: 'idea',
-        desc: `Das ist die Idee`,
-    },
-    {
-        label: 'Lösung',
-        value: 'solution',
-        desc: `Das ist die Lösung`,
-    },
-    {
-        label: 'Dokumentation',
-        value: 'documentation',
-        desc: `Das ist die Dokumentation`,
-    },
-    {
-        label: 'Kommentare',
-        value: 'comments',
-        desc: `Hier sind Kommentare`,
-    },
-];
 
-interface BearState {
-    activeValue: string;
-    setActiveValue: (value: string) => void;
-}
+const TaskTwo = () => {
+    return (
+        <div>
+            <p className='font-bold'>Aufgabenstellung:</p>
+            <p>Das ist ESA 2</p>
+            <br />
+            <p>
+                Es ist Ihnen überlassen, ob Sie die Bilder auf mehrere Dateien
+                verteilen, oder alle Bilder der Animation als Sprite-Sheet, also
+                in einer Datei anordnen.
+            </p>
+            <br />
+            <p className='font-bold'>Erweiterungen: </p>
+            <div>
+                <ol>
+                    <li>
+                        1: Außer einer Scheibe kreieren und animieren Sie noch
+                        ein anderes Objekt Ihrer Wahl als Sprite-Sheet, z. B.
+                        einen hüpfenden Hasen.
+                    </li>
+                    <br />
+                    <li>
+                        2: Die Scheibe dreht sich automatisch mit Taste a, dazu
+                        muss diese kontinuierlich animiert werden (die Animation
+                        sollte bei einem weiteren Drücken von a auch wieder
+                        angehalten werden).
+                    </li>
+                </ol>
+            </div>
+            <br />
+            <p>
+                <span className='font-bold'>Interaktion:</span> Auf den
+                Tastendruck l oder r soll sich die Scheibe augenscheinlich ein
+                Stück nach links beziehungsweise nach rechts drehen. Zum Drehen
+                muss man wiederholt drücken (mit l und r soll es nur also einen
+                Schritt/ ein Bild weiter gehen.). Das automatische Drehen und
+                Stoppen für die Erweiterung erfolgt über die Taste a. Alternativ
+                können Sie die Funktionalität auf Button abbilden. Beachten Sie
+                dabei Human/Mensch-Computer-Interaction (HCI) Kriterien beim
+                Interaktionsdesign: ISO 9241-11 Anforderungen an die
+                Gebrauchstauglichkeit und die ISO 9241-110 zu
+                Interaktionsprinzipien.
+            </p>
+        </div>
+    );
+};
 
-const useBearStore = create<BearState>()((set) => ({
-    activeValue: '',
-    setActiveValue: (value) => set(() => ({ activeValue: value })),
-}));
+const TaskThree = () => {
+    return (
+        <div>
+            <p className='font-bold'>Aufgabenstellung:</p>
+            <p>Das ist ESA 3</p>
+            <br />
+            <p>
+                Es ist Ihnen überlassen, ob Sie die Bilder auf mehrere Dateien
+                verteilen, oder alle Bilder der Animation als Sprite-Sheet, also
+                in einer Datei anordnen.
+            </p>
+            <br />
+            <p className='font-bold'>Erweiterungen: </p>
+            <div>
+                <ol>
+                    <li>
+                        1: Außer einer Scheibe kreieren und animieren Sie noch
+                        ein anderes Objekt Ihrer Wahl als Sprite-Sheet, z. B.
+                        einen hüpfenden Hasen.
+                    </li>
+                    <br />
+                    <li>
+                        2: Die Scheibe dreht sich automatisch mit Taste a, dazu
+                        muss diese kontinuierlich animiert werden (die Animation
+                        sollte bei einem weiteren Drücken von a auch wieder
+                        angehalten werden).
+                    </li>
+                </ol>
+            </div>
+            <br />
+            <p>
+                <span className='font-bold'>Interaktion:</span> Auf den
+                Tastendruck l oder r soll sich die Scheibe augenscheinlich ein
+                Stück nach links beziehungsweise nach rechts drehen. Zum Drehen
+                muss man wiederholt drücken (mit l und r soll es nur also einen
+                Schritt/ ein Bild weiter gehen.). Das automatische Drehen und
+                Stoppen für die Erweiterung erfolgt über die Taste a. Alternativ
+                können Sie die Funktionalität auf Button abbilden. Beachten Sie
+                dabei Human/Mensch-Computer-Interaction (HCI) Kriterien beim
+                Interaktionsdesign: ISO 9241-11 Anforderungen an die
+                Gebrauchstauglichkeit und die ISO 9241-110 zu
+                Interaktionsprinzipien.
+            </p>
+        </div>
+    );
+};
+
+type ComponentType = {
+    [key: string]: JSX.Element | undefined;
+};
 
 export const GraphicalVisualizationTabs = () => {
-    const activeValue: string = useBearStore((state) => state.activeValue);
-    activeValue && window.console.log(activeValue);
+    const activeValue: string = useActiveEsaStore((state) => state.activeEsa);
+
+    const components: ComponentType = {
+        TaskOne: <TaskOne />,
+        TaskTwo: <TaskTwo />,
+        TaskThree: <TaskThree />,
+    };
+
+    const data = [
+        {
+            label: 'Aufgabenstellung',
+            value: 'task',
+            desc: components[activeValue],
+        },
+        {
+            label: 'Idee',
+            value: 'idea',
+            desc: `Das ist die Idee`,
+        },
+        {
+            label: 'Lösung',
+            value: 'solution',
+            desc: `Das ist die Lösung`,
+        },
+        {
+            label: 'Dokumentation',
+            value: 'documentation',
+            desc: `Das ist die Dokumentation`,
+        },
+        {
+            label: 'Kommentare',
+            value: 'comments',
+            desc: `Hier sind Kommentare`,
+        },
+    ];
     return (
         <UnderlineTabs
             data={data}
             initialActiveTab={'task'}
-            menu={<TabOptions />}
+            tabOptions={<TabOptions />}
         />
     );
 };
 
 const TabOptions: FC = () => {
-    const [activeValue, setActiveValue] = useState('ESA 1');
-    const storeValue = useBearStore((state) => state.setActiveValue);
+    const [activeValue, setActiveValue] = useState('TaskOne');
+    const storeValue = useActiveEsaStore((state) => state.setActiveEsa);
+    useEffect(() => {
+        storeValue(activeValue);
+    }, [storeValue, activeValue]);
+
     return (
         <div className='w-72'>
             <Select
@@ -123,10 +221,20 @@ const TabOptions: FC = () => {
                     value && storeValue(value);
                 }}
             >
-                <Option value='ESA 1'>ESA 1</Option>
-                <Option value='ESA 2'>ESA 2</Option>
-                <Option value='ESA 3'>ESA 3</Option>
+                <Option value='TaskOne'>ESA 1</Option>
+                <Option value='TaskTwo'>ESA 2</Option>
+                <Option value='TaskThree'>ESA 3</Option>
             </Select>
         </div>
     );
 };
+
+interface activeEsaStore {
+    activeEsa: string;
+    setActiveEsa: (value: string) => void;
+}
+
+const useActiveEsaStore = create<activeEsaStore>()((set) => ({
+    activeEsa: '',
+    setActiveEsa: (value) => set(() => ({ activeEsa: value })),
+}));
