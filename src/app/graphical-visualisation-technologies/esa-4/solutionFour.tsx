6f8d9d103ec7402c0184822b38750a02adca4583
Iskender Dilaver <iskender.dilaver@gmail.com>
2023-11-12 16:36:06 +0100

    fix: better alignment

--- src/app/graphical-visualisation-technologies/esa-4/solutionFour.tsx
+++ src/app/graphical-visualisation-technologies/esa-4/solutionFour.tsx
@@ -9,9 +9,9 @@ const SolutionFour: React.FC = () => {
     const [gitterVisible, setGitterVisible] = useState<boolean>(true);
     const [areaVisible, setAreaVisible] = useState<boolean>(true);
     return (
-        <div className='grid grid-cols-3 gap-4'>
+        <div className='grid grid-cols-3 gap-4 justify-items-center'>
             <Donut gitterVisible={gitterVisible} areaVisible={areaVisible} />
-            <div className='grid gap-4 mx-auto'>
+            <div className='grid gap-4'>
                 <AntisymmetricTorus
                     gitterVisible={gitterVisible}
                     areaVisible={areaVisible}
